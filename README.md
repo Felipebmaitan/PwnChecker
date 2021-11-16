846a7104ef5b0cc57771295377f48cf2762149df
Felipe <felipebmaitan@protonmail.com>
2021-11-16 18:36:57 -0300

    Finished the app with a little ugly hack

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pwnchecker - a simple script to check if your passwords are compromised.
 
-PwnChecker is a simple script written in Python that uses [HaveIBeenPwned](https://haveibeenpwned.com/API/v3#PwnedPasswords) API to search for breaches that contain your password. The script hashes the password with SHA-1 and **NEVER** sends the full hash to the API, only the first 5 characters.
+PwnChecker is a simple script written in Python that uses [HaveIBeenPwned API](https://haveibeenpwned.com/API/v3#PwnedPasswords) to search for breaches that contain your password. The script hashes the password with SHA-1 and **NEVER** sends the full hash to the API, only the first 5 characters.
--- check.py
+++ check.py
@@ -1,19 +1,29 @@
 import hashlib
 import requests
 
-# Defines the variables and already hash the password inputed by the user.
+# Defines the variables and hashes the input password.
 
-passwd = '123'
-# passwd = input('Type the password you want to test here: ')
+passwd = input('Type the password you want to test here: ')
 passhash = hashlib.sha1(passwd.encode('UTF-8')).hexdigest().upper()
 api = 'https://api.pwnedpasswords.com/range/'
 
-# Contacts the API and informs the user if there was any errors.
+# Contacts the API and informs the user if there were any errors.
 
 resp = requests.get(api + passhash[:5])
 if resp.status_code != 200:
     print('An error happened while contacting the API.')
 
-# TODO:
-#       - Sort to the request and find the hash of the password provided
-#       - Return to the user the hash of their password and how many times it was leaked.
+# Searches the API response for the password hash and if found returns the hash of the password and 
+# the amount of times it was leaked.
+
+for line in resp.text.splitlines():
+    hash = line.split(':')[0]
+    n = line.split(':')[1]
+    if hash == passhash[5:]: 
+        print ('Your password was found in one of our databases. \nThe hash of your password is: {}. \nIt was leaked {} times.'.format(passhash, n))
+# At this point I was so tried of trying to find the right way to do that that I ended up with this quick fix. I may fix this in the future if there is any way to do so.
+        flag = True
+try:
+    flag
+except NameError: 
+    print('Your password was not found on our databases.')
