05183ab648212251758920b6958b2febc86702e2
Felipe <felipebmaitan@protonmail.com>
2022-01-25 16:08:44 -0300

    Basic comments change and now shows the hash even if the password was not leaked

--- check.py
+++ check.py
@@ -21,9 +21,11 @@ for line in resp.text.splitlines():
     n = line.split(':')[1]
     if hash == passhash[5:]: 
         print ('Your password was found in one of our databases. \nThe hash of your password is: {}. \nIt was leaked {} times.'.format(passhash, n))
-# At this point I was so tried of trying to find the right way to do that that I ended up with this quick fix. I may fix this in the future if there is any way to do so.
+
+# At this point I was so tried of trying to find the right way to do that that I ended up with this quick fix. 
+# I may fix this in the future if there is any way to do so.
         flag = True
 try:
     flag
 except NameError: 
-    print('Your password was not found on our databases.')
+    print('Your password was not found on our databases. \nThe hash of your password is: {}'.format(passhash))
